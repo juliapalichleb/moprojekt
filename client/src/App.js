7038b28441be77f95ae28867352488e817909818
Julia Palichleb <juliapalichlebcc@gmail.com>
2022-08-18 15:08:14 +0200

    init nest

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+const [data, setData] = useState();
+
+  const handleOnClick = () => {
+    axios.get('http://localhost:5000/api').then( ({data}) => setData(data) );
+    console.log(data)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={handleOnClick}>KlIK</button>
+        <p>{data}</p>
     </div>
   );
 }

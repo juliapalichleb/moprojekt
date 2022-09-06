8ec9f2d29935dc0d936d2740fa0289bd905520ba
Julia Palichleb <juliapalichlebcc@gmail.com>
2022-09-06 16:44:02 +0200

    style

---
+++ client/src/pages/index.js
@@ -0,0 +1,9 @@
+import FirstPage from "./FirstPage/FirstPage";
+import SecondPage from "./SecondPage/SecondPage";
+import ThirdPage from "./ThirdPage/ThirdPage";
+
+export {
+    FirstPage,
+    SecondPage,
+    ThirdPage
+}
